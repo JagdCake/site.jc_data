58937a9207950d8926fadc8a076fb6ff8683a15e
Ivo Angelov <jagdcake@outlook.com>
2018-10-08 17:35:47 +0300

    Remove the unnecessary parts of the IDs

--- main.sh
+++ main.sh
@@ -2,5 +2,5 @@
 
 html_file=~/Documents/web_dev/3_my_sites/iwam/index.html
 
-rg -o -N -e "(tt\d{7}/\?)|(tt\d{7}/\")" "$html_file"
+rg -o -N -e "(tt\d{7}/\?)|(tt\d{7}/\")" "$html_file" | awk -F'/' '{ print $1 }'
 
