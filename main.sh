8e6d39ff69aa6d810499f8be9de344a71e0725e7
Ivo Angelov <jagdcake@outlook.com>
2018-10-08 18:45:12 +0300

    Organize data extraction commands into functions

--- main.sh
+++ main.sh
@@ -6,15 +6,21 @@ mode="$1"
 html_file=~/Documents/web_dev/3_my_sites/iwam/index.html
 basics_file=./title.basics.tsv
 
-
-if [ "$mode" == 'regen' ]; then
+find_ids() {
     # find all IMDb IDs
     ids=($(rg -o -N -e "(tt\d{7}/\?)|(tt\d{7}/\")" "$html_file" | awk -F'/' '{ print $1 }'))
+}
 
+find_genres() {
     for id in "${ids[@]}"; do
         # finds the main genre
        rg "$id" "$basics_file" | awk -F"\t" '{ print $9 }' | awk -F',' '{ print $1 }' >> ./genres
     done
+}
+
+if [ "$mode" == 'regen' ]; then
+    find_ids
+    find_genres
 fi
 
 # display the number of movies from every genre
