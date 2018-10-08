172d1db1d599f157ee63d2f4f66a8d1d033ca685
Ivo Angelov <jagdcake@outlook.com>
2018-10-08 17:57:26 +0300

    Save the main genre of every movie to a file

--- main.sh
+++ main.sh
@@ -6,4 +6,8 @@ basics_file=./title.basics.tsv
 # find all IMDb IDs
 ids=($(rg -o -N -e "(tt\d{7}/\?)|(tt\d{7}/\")" "$html_file" | awk -F'/' '{ print $1 }'))
 
+for id in "${ids[@]}"; do
+    # finds the main genre
+   rg "$id" "$basics_file" | awk -F"\t" '{ print $9 }' | awk -F',' '{ print $1 }' >> ./genres
+done
 
