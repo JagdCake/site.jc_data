0f2259e60c2548af425e04f8df6dfde5dbcaa9d3
Ivo Angelov <jagdcake@outlook.com>
2018-10-08 17:49:07 +0300

    Set path to basic info file

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 html_file=~/Documents/web_dev/3_my_sites/iwam/index.html
+basics_file=./title.basics.tsv
 
 # find all IMDb IDs
 ids=($(rg -o -N -e "(tt\d{7}/\?)|(tt\d{7}/\")" "$html_file" | awk -F'/' '{ print $1 }'))
