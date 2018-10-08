81ff53d23e2bf50a10d63334486459e1ba0edd53
Ivo Angelov <jagdcake@outlook.com>
2018-10-08 18:02:42 +0300

    Start data extraction process only if needed

--- main.sh
+++ main.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# enter regen to regenerate the data
+mode="$1"
+
 html_file=~/Documents/web_dev/3_my_sites/iwam/index.html
 basics_file=./title.basics.tsv
 
-# find all IMDb IDs
-ids=($(rg -o -N -e "(tt\d{7}/\?)|(tt\d{7}/\")" "$html_file" | awk -F'/' '{ print $1 }'))
 
-for id in "${ids[@]}"; do
-    # finds the main genre
-   rg "$id" "$basics_file" | awk -F"\t" '{ print $9 }' | awk -F',' '{ print $1 }' >> ./genres
-done
+if [ "$mode" == 'regen' ]; then
+    # find all IMDb IDs
+    ids=($(rg -o -N -e "(tt\d{7}/\?)|(tt\d{7}/\")" "$html_file" | awk -F'/' '{ print $1 }'))
+
+    for id in "${ids[@]}"; do
+        # finds the main genre
+       rg "$id" "$basics_file" | awk -F"\t" '{ print $9 }' | awk -F',' '{ print $1 }' >> ./genres
+    done
+fi
 
