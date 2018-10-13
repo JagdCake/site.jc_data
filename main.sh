20aca807674f75770ef03bd228fb583bd9d149b4
Ivo Angelov <jagdcake@outlook.com>
2018-10-13 17:33:11 +0300

    Fix wrong variable name

--- main.sh
+++ main.sh
@@ -41,7 +41,7 @@ download_data() {
 
 find_movie_ids() {
     # find all IMDb IDs
-    rg -o -N -e "(tt\d{7}/\?)|(tt\d{7}/\")" "$html_file" | awk -F'/' '{ print $1 }' >> "$movie_ids_file"
+    rg -o -N -e "(tt\d{7}/\?)|(tt\d{7}/\")" "$movies_html_file" | awk -F'/' '{ print $1 }' >> "$movie_ids_file"
 }
 
 select_the_property() {
