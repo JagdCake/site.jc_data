7adb812cee292c8a11252447f2058d14b0217e20
Ivo Angelov <jagdcake@outlook.com>
2018-11-05 15:46:21 +0200

    Create function to add a single piece of data to HTML file

--- visualize_data.sh
+++ visualize_data.sh
@@ -2,3 +2,10 @@ source main.sh visualize
 
 index_file=~/Documents/web_dev/3_my_sites/data/index.html
 
+add_data() {
+    keyword="$1"
+    data="$2"
+
+    sed -i "s/$keyword/$data/" "$index_file"
+}
+
