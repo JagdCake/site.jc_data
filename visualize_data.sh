d16b1874dc45f9d34698b635bc5c469ef52ecab7
Ivo Angelov <jagdcake@outlook.com>
2018-11-05 16:06:24 +0200

    Create function to add data to specific site sections

--- visualize_data.sh
+++ visualize_data.sh
@@ -24,3 +24,13 @@ add_array_data() {
     data_arr=()
 }
 
+add_data_to_section() {
+    section="$1"
+
+    if [ "$section" == 'dates' ]; then
+        add_data "date_last_update_machine" "$date_last_update_machine"
+        add_data "date_last_update_human" "$date_last_update_human"
+        add_data "movie_number" "$movie_number"
+    fi
+}
+
