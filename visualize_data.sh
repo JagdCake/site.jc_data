9088d5413cbbe18aac60140620f360c972a67a01
Ivo Angelov <jagdcake@outlook.com>
2018-11-05 15:48:20 +0200

    Create function to add an array of data to HTML file

--- visualize_data.sh
+++ visualize_data.sh
@@ -9,3 +9,18 @@ add_data() {
     sed -i "s/$keyword/$data/" "$index_file"
 }
 
+add_array_data() {
+    keyword="$1"
+    # Source: https://stackoverflow.com/a/16462240/8980616
+    shift 1
+    data=("$@")
+
+    for i in "${data[@]}"; do
+        data_arr+=("<li>${i}<\/li>")
+    done
+
+    sed -i "s/$keyword/$(echo -n ${data_arr[@]})/" "$index_file"
+
+    data_arr=()
+}
+
