a1ca4abe3c5c3c06e1aac5c2765ad031ddb259bd
Ivo Angelov <jagdcake@tuta.io>
2019-04-11 08:52:08 +0300

    Find all movie data

--- symfony/src/Controller/MovieDataController.php
+++ symfony/src/Controller/MovieDataController.php
@@ -4,16 +4,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
 
 class MovieDataController extends AbstractController
 {
     /**
      * @Route("/", name="index")
      */
-    public function index()
+    public function index(MovieRepository $repo): Response
     {
         return $this->render('movie_data/index.html.twig', [
-            'controller_name' => 'MovieDataController',
+            'movie_data' => $repo->findAll(),
         ]);
     }
 }
