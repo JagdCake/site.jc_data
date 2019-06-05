84728b61ff540e4f31e25ffe7f8b55bac2499e90
Ivo Angelov <jagdcake@tuta.io>
2019-06-05 11:47:06 +0300

    Create helper functions to calculate time from minutes

---
+++ tests/Repository/MovieRepositoryTestHelpers.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests\Repository;
+
+class MovieRepositoryTestHelpers
+{
+    public static function days(int $minutes): int
+    {
+        return round($minutes / 1440);
+    }
+
+    public static function hours(int $minutes): int
+    {
+        return floor($minutes / 60);
+    }
+
+    public static function remainingMinutes(int $minutes): int
+    {
+        return $minutes % 60;
+    }
+}
